40f8c4d4d8c8a02c0449cf9c50e9b662ee2f6266
Tobias Wilken <tooangel@tooangel.de>
2018-07-23 10:20:11 +0200

    Enforce ssl

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const rimraf = require('rimraf');
 
 const app = express();
 
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+
 app.use(fileUpload());
 
 app.get('/', function(req, res) {
